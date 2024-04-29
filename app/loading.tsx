cf8e4ef2e68882b8499ae1b2c1ce573f897a1e8e
sai24121994 <sai24121994@gmail.com>
2024-04-29 11:46:27 +0530

    removed css file

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 import { Layout } from "./components/Layout";
-import styles from "./ui/spinner.module.css";
 
 const Loading = () => {
   return (
     <Layout>
       <div className="flex min-w-full min-h-full justify-center items-center">
-      <span className={styles.loader}></span>
+      <span>Loading...</span>
       </div>
     </Layout>
   );
