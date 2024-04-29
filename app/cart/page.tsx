d1e9bcd0d9280b1f779e2e5330456bb4eddef464
sai24121994 <sai24121994@gmail.com>
2024-04-29 21:19:12 +0530

    sample api tested

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -2,6 +2,26 @@ import React from "react";
 import { Layout } from "../components/Layout";
 
 const page = () => {
+  async function fetchData() {
+    try {
+      const response = await fetch('http://localhost:3000/lib/api',{
+        method: "POST",
+        body: JSON.stringify({
+          "data" : "test api"
+        }),
+      }); // Replace '/api/test' with your actual API route path
+      if (!response.ok) {
+        throw new Error(`API call failed with status ${response.status}`);
+      }
+      const data = await response.json();
+      console.log(data); // This will log { message: "This is a test API endpoint!" }
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  }
+
+  fetchData();
+
   return (
     <Layout>
       <div>page</div>
